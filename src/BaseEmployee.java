a481526b45e780e296cc03c7ddbddcf07c6f268b
OlenaSMarchenko <olena.smarchenko@gmail.com>
2021-10-28 15:40:33 -0700

    "add solved task 17.1"

---
+++ src/BaseEmployee.java
@@ -0,0 +1,52 @@
+public abstract class BaseEmployee {
+
+    private int age;
+    private String name;
+    private String gender;
+    private int daySalary;
+
+    public BaseEmployee(int age, String name, String gender, int daySalary) {
+        this.age = age;
+        this.name = name;
+        this.gender = gender;
+        this.daySalary = daySalary;
+    }
+
+
+
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getDaySalary() {
+        return daySalary;
+    }
+    public void setDaySalary(int daySalary) {
+        this.daySalary = daySalary;
+    }
+
+    public int getSalary(Month[] monthArray) {
+        int sum = 0;
+        for (int i = 0; i < monthArray.length; i++) {
+            sum += getDaySalary() * monthArray[i].getWorkDays();
+        }
+        return  sum;
+    }
+}
--- src/Employee.java
+++ src/Employee.java
@@ -1,50 +1,6 @@
-public class Employee {
+public final class Employee extends BaseEmployee {
 
-    private int age;
-    private String name;
-    private String gender;
-    private int daySalary;
-
-    public Employee (int age, String name, String gender, int daySalary) {
-        this.age = age;
-        this.name = name;
-        this.gender = gender;
-        this.daySalary = daySalary;
-    }
-
-    public int getAge() {
-        return age;
-    }
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-    public int getDaySalary() {
-        return daySalary;
-    }
-    public void setDaySalary(int daySalary) {
-        this.daySalary = daySalary;
-    }
-
-    public int getSalary(Month[] monthArray) {
-        int sum = 0;
-        for (int i = 0; i < monthArray.length; i++) {
-            sum += getDaySalary() * monthArray[i].getWorkDays();
-        }
-        return  sum;
+    public Employee(int age, String name, String gender, int daySalary) {
+        super(age, name, gender, daySalary);
     }
 }
